c3202453c3e5ffed7e0ff84f65680b09928c13cc
Tang <tangz@tumblr.com>
2015-09-28 12:00:44 -0400

    add podfile and add oauth

--- Roomblr/AppDelegate.swift
+++ Roomblr/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var storyboard = UIStoryboard(name: "Main", bundle: nil)
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if User.currentUser != nil {
+            // go to loggedin screen
+            var vc = storyboard.instantiateViewControllerWithIdentifier("GroupNavController") as! UINavigationController
+            window?.rootViewController = vc
+        }
         return true
     }
 
@@ -39,6 +44,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        
+        TumblrClient.sharedInstance.openURL(url)
+        
+        return true
     }
 
 
---
+++ Roomblr/User.swift
@@ -0,0 +1,58 @@
+//
+//  User.swift
+//  Roomblr
+//
+//  Created by Tang Zhang on 9/28/15.
+//  Copyright © 2015 tumblr. All rights reserved.
+//
+
+import UIKit
+
+var _currentUser: User?
+let currentUserKey = "CurrentUserKey"
+let userDidLoginNotification = "userDidLoginNotification"
+let userDidLogoutNotification = "userDidLogoutNotification"
+
+class User: NSObject {
+    var name: String?
+    var dic: NSDictionary
+    
+    init(dic: NSDictionary) {
+        self.dic = dic
+        name = dic["name"] as? String
+    }
+    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil {
+                var data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+                if data != nil {
+                    do {
+                        var dic = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                        _currentUser = User(dic: dic)
+                    }catch let error as NSError {
+                        print(error)
+                    }
+            
+                }
+            }
+            return _currentUser
+        }
+        
+        set(user) {
+            _currentUser = user
+            if _currentUser != nil {
+                do {
+                    var data = try NSJSONSerialization.dataWithJSONObject(user!.dic, options: NSJSONWritingOptions.init())
+                    NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+                }catch let error as NSError {
+                    print(error)
+                }
+            } else {
+                NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+            }
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+
+}
