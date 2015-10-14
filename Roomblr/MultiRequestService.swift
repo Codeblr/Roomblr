afd369bdfa6dbce5868e700757fec37eab40d9b1
Jasen Salcido <jpsalcid@gmail.com>
2015-10-14 12:56:48 -0700

    added multiple class for git blog likes

---
+++ Roomblr/MultiRequestService.swift
@@ -0,0 +1,21 @@
+//
+//  MultiRequestService.swift
+//  Roomblr
+//
+//  Created by Jasen Salcido on 10/14/15.
+//  Copyright © 2015 tumblr. All rights reserved.
+//
+
+import Foundation
+
+
+class MultiRequestService {
+    var apiMethod: AnyObject
+    
+    init(apiMethod: AnyObject) {
+        self.apiMethod = apiMethod
+    }
+    
+    
+    
+}
--- Roomblr/RoomblrUtility.swift
+++ Roomblr/RoomblrUtility.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 let NUMBER_OF_TOP_TAGS = 1
+let NUMBER_OF_MAX_REQUESTS = 5
 
 class RoomblrUtility {
     
@@ -19,35 +20,52 @@ class RoomblrUtility {
         return Static.instance
     }
     
+    
     // get top x tags from liked posts
     func getTopTagsFromPosts(completion: (tags: [String]?, error: NSError?) -> ()) {
         var tagDictionary = [String: Int]()
+        var topTagsFromPostsQueue = 0
         // get all the PFUsers
         ParseClient.sharedInstance.fetchAllPFUseres { (pfUsers: [PFUser]?, error: NSError?) -> () in
             if error == nil {
                 // TODO change this to get all likes from all users and collect top tags
-                var randomNumber = Int(arc4random()) % (pfUsers!.count - 1)
-                var pfUser = pfUsers![randomNumber]
+//                var randomNumber = Int(arc4random()) % (pfUsers!.count - 1)
+//                var pfUser = pfUsers![randomNumber]
                 
-                // fetch likes
-                TumblrClient.sharedInstance.getBlogLikesPosts(pfUser.username!, completion: { (posts, error) -> () in
-                    if error == nil {
-                        for post in posts! {
-                            // put tags in dictionary
-                            for tag in post.tags! {
-                                if tagDictionary[tag] == nil {
-                                    tagDictionary[tag] = 1
-                                } else {
-                                    tagDictionary[tag] = tagDictionary[tag]! + 1
+                topTagsFromPostsQueue = pfUsers!.count
+                
+                for var i = 0; i < pfUsers!.count; i++ {
+                    // until you get to max asyn requests
+                    if i > NUMBER_OF_MAX_REQUESTS {
+                        break;
+                    }
+                    var pfUser = pfUsers![i]
+                    
+                    // fetch the user's blog likes
+                    TumblrClient.sharedInstance.getBlogLikesPosts(pfUser.username!, completion: { (posts, error) -> () in
+                        topTagsFromPostsQueue--
+                        if error == nil {
+                            for post in posts! {
+                                // put tags in dictionary
+                                for tag in post.tags! {
+                                    if tagDictionary[tag] == nil {
+                                        tagDictionary[tag] = 1
+                                    } else {
+                                        tagDictionary[tag] = tagDictionary[tag]! + 1
+                                    }
                                 }
                             }
+                            
+                            // check if last in queue
+                            if topTagsFromPostsQueue == 0 {
+                                let tags = self.sortMaxTags(tagDictionary) as! [String]
+                                completion(tags: tags, error: nil)
+                            }
+                            
                         }
-                        // now sort tags in an array
-                        
-                        let tags = self.sortMaxTags(tagDictionary) as! [String]
-                        completion(tags: tags, error: nil)
-                    }
-                })
+                    })
+                }
+                
             } else {
                 // error
             }
